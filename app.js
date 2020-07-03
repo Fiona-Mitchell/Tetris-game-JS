75e1d18758e9618935c1ee4921f914f2c702ad00
Fiona-Mitchell <fionamitchelldesign@gmail.com>
2020-07-03 11:05:42 +0100

    First tetrimino - done

--- app.js
+++ app.js
@@ -1,57 +1,52 @@
 document.addEventListener('DOMContentLoaded', () => {
   const grid = document.querySelector('.grid')
   let squares = Array.from(document.querySelectorAll('.grid div'))
-  const ScoreDisplay = document.querySelector('#score')
-  const StartBtn = document.querySelector('#start-button')
+  const scoreDisplay = document.querySelector('#score')
+  const startBtn = document.querySelector('#start-button')
   const width = 10
 
   //shapes of the tetrominoes
-  const lTetrimono = [
+  const lTetrimino = [
     [1, width+1, width*2+1, 2],
-    [width, width+1, width+2, width*2],
+    [width, width+1, width+2, width*2+2],
     [1, width+1, width*2+1, width*2],
-    [width, width*2, width *2+1, width*2+2]
+    [width, width*2, width*2+1, width*2+2]
   ]
 
   const zTetrimino = [
-    [0, width, width+1, width*2+1],
-    [width+1, width+2, width*2, width*2+1],
-    [0, width, width+1, width*2+1],
-    [width+1, width+2, width*2, width*2+1],
+    [0,width,width+1,width*2+1],
+    [width+1, width+2,width*2,width*2+1],
+    [0,width,width+1,width*2+1],
+    [width+1, width+2,width*2,width*2+1]
   ]
 
   const tTerimino = [
-    [1, width, width+1, width+2],
-    [1, width+1, width+2, width*2+1],
-    [width, width+1, width+2, width*2+1],
-    [1, width, width+1, width*2+1]
+    [1,width,width+1,width+2],
+    [1,width+1,width+2,width*2+1],
+    [width,width+1,width+2,width*2+1],
+    [1,width,width+1,width*2+1]
   ]
 
   const oTetrimino = [
-    [0, 1, width, width+1],
-    [0, 1, width, width+1],
-    [0, 1, width, width+1],
-    [0, 1, width, width+1]
+    [0,1,width,width+1],
+    [0,1,width,width+1],
+    [0,1,width,width+1],
+    [0,1,width,width+1]
   ]
 
   const iTetrimino = [
-    [1, width+1, width*2+1, width*3+1],
-    [width, width+1, width+2, width+3],
-    [1, width+1, width*2+1, width*3+1],
-    [width, width+1, width+2, width+3]
+    [1,width+1,width*2+1,width*3+1],
+    [width,width+1,width+2,width+3],
+    [1,width+1,width*2+1,width*3+1],
+    [width,width+1,width+2,width+3]
   ]
 
-  const theTetriminoes = [lTetrimono, zTetrimino, tTerimino, oTetrimino, iTetrimino]
+  const theTetriminoes = [lTetrimino, zTetrimino, tTerimino, oTetrimino, iTetrimino]
 
 //start position and starting angle
   let currentPosition = 4
-  let currentRotation = 0
+  let current = theTetriminoes[0][0]
 
-
-//make a tetrimino be chosen randomly
-let random = Math.floor(Math.random()*theTetriminoes.length)
-
-//draw the first tetrimino
 
 function draw() {
   current.forEach(index => {
@@ -59,22 +54,6 @@ function draw() {
   })
 }
 
-//undraw the tetrimino
-function undraw(){
-  current.forEach(index => {
-    squares[currentPosition + index].classList.remove('tetrimino')
-  })
-}
-
-
-//
-
-
-
-
-
-
-
-
+draw()
 
 })
